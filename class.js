083ccdef96364080612eb92cdff876d9f5e443b8
Shaun Furman <shaunfurman08@gmail.com>
2020-07-14 10:35:05 -0500

    double check commit

---
+++ class.js
@@ -0,0 +1,55 @@
+class Traveler {
+    constructor(name) {
+        this.name = name
+        this.food = 1
+        this.isHealthy = true
+    }
+    hunt() {
+        this.food += 2
+
+    }
+    eat() {
+
+        if (this.food === 0) {
+            this.isHealthy = false
+        }
+        else {
+            this.food -= 1
+        }
+
+    }
+}
+class Wagon {
+    constructor(capacity) {
+        this.capacity = capacity
+        this.passengers = []
+
+    }
+    getAvailableSeatCount() {
+
+        const availableSeats = this.capacity - this.passengers.length
+        return availableSeats
+        console.log(availableSeats)
+    } //– Returns the number of empty seats, determined by the capacity set when the wagon was created, subtracted by the number of passengers currently on board.
+    join(traveler) {
+        if (this.getAvailableSeatCount() > 0) {
+            this.passengers.push(traveler)
+            console.log(traveler)
+        }
+    }
+    shouldQuarantine() {
+        // if (this.passengers.some(this.isHealthy) === false) {
+        //     return true
+        // } else {
+        //     return false
+      
+        let q = this.passengers.some(traveler => this.passengers.food)
+        return true
+    }    //- Returns true if there is at least one unhealthy person in the wagon. Return false if not.
+
+    totalFood() {
+            let foodCount = this.passengers.map(traveler => this.passengers = this.food)
+        return foodCount //variables?
+        console.log(foodCount)
+    }}
+    //- Returns the total amount of food among all passengers in the wagon.
